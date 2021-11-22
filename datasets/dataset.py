a8961b962486a9f99b42c668a80a94cbe819af2f
barbafrank <francesco.barbato@unipd.it>
2021-11-22 17:53:39 +0100

    fixed small bugs

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -34,29 +34,32 @@ class BaseDataset(Dataset):
 
     def resize_and_crop(self, rgb=None, gt=None, depth=None):
         if self.resize_to is not None:
-            rgb = cv.resize(rgb, self.resize_to, interpolation=cv.INTER_AREA) if rgb is not None # usually images are downsized, best results obtained with inter_area
-            gt = cv.resize(gt, self.resize_to, interpolation=cv.INTER_NEAREST_EXACT) if gt is not None # labels must be mapped as-is
-            depth = cv.resize(depth, self.resize_to, interpolation=cv.INTER_NEAREST_EXACT) if depth is not None
+            if rgb is not None: rgb = cv.resize(rgb, self.resize_to, interpolation=cv.INTER_AREA) # usually images are downsized, best results obtained with inter_area
+            if gt is not None: gt = cv.resize(gt, self.resize_to, interpolation=cv.INTER_NEAREST_EXACT) # labels must be mapped as-is
+            if depth is not None: depth = cv.resize(depth, self.resize_to, interpolation=cv.INTER_NEAREST_EXACT)
 
         if self.crop_size is not None:
-            H, W, _ = rgb.shape
-            dh, dw = H-self.crop_size[1], H-self.crop_size[0]
-            assert dh>=0 and dw >= 0, "Incompatible crop size: (%d, %d), images have dimensions: (%d, %d)"%(self.crop_size[0], self.crop_size[1], W, H)
-            h0, w0 = random.randint(0, dh) if dh>0 else 0, random.randint(0, dw) if dw>0 else 0
-            rgb = (rgb[h0:h0+self.crop_size[1], w0:w0+self.crop_size[0], ...]).copy() if rgb is not None
-            gt = (gt[h0:h0+self.crop_size[1], w0:w0+self.crop_size[0], ...]).copy() if gt is not None
-            depth = (depth[h0:h0+self.crop_size[1], w0:w0+self.crop_size[0], ...]).copy() if depth is not None
+            if rgb is not None: H, W, _ = rgb.shape
+            if gt is not None: H, W = gt.shape
+            if depth is not None: H, W = depth.shape
+            if not (rgb is None and gt is None and depth is None):
+                dh, dw = H-self.crop_size[1], W-self.crop_size[0]
+                assert dh>=0 and dw >= 0, "Incompatible crop size: (%d, %d), images have dimensions: (%d, %d)"%(self.crop_size[0], self.crop_size[1], W, H)
+                h0, w0 = random.randint(0, dh) if dh>0 else 0, random.randint(0, dw) if dw>0 else 0
+            if rgb is not None: rgb = (rgb[h0:h0+self.crop_size[1], w0:w0+self.crop_size[0], ...]).copy()
+            if gt is not None: gt = (gt[h0:h0+self.crop_size[1], w0:w0+self.crop_size[0], ...]).copy()
+            if depth is not None: depth = (depth[h0:h0+self.crop_size[1], w0:w0+self.crop_size[0], ...]).copy()
 
         return rgb, gt, depth
 
     def data_augment(self, rgb=None, gt=None, depth=None):
         if self.kwargs['flip'] and random.random()<.5:
-            rgb = (rgb[:,::-1,...]).copy() if rgb is not None
-            gt = (gt[:,::-1,...]).copy() if gt is not None
-            depth = (gt[:,::-1,...]).copy() if depth is not None
+            if rgb is not None: rgb = (rgb[:,::-1,...]).copy()
+            if gt is not None: gt = (gt[:,::-1,...]).copy()
+            if depth is not None: depth = (gt[:,::-1,...]).copy()
         if self.kwargs['gaussian_blur']:
             sigma = random.random()*self.kwargs['blur_mul']
-            rgb = cv.GaussianBlur(rgb, (0,0), sigma) if rgb is not None
+            if rgb is not None: rgb = cv.GaussianBlur(rgb, (0,0), sigma)
             # not sure if it makes sense....
             #depth = cv.GaussianBlur(depth, (0,0), sigma) if depth is not None
         return rgb, gt, depth
